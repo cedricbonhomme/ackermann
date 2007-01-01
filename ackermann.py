758593c9924d7c6a00092af643c4477ea2dcb749
cedricbonhomme <cedricbonhomme@localhost>
2007-01-01 14:33:19 +0100

    Improved Python version of Ackermann added.

--- ackermann.py
+++ ackermann.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+
 
 
 def ackermann_1(m, n):
@@ -7,3 +10,23 @@ def ackermann_1(m, n):
         return ackermann(m - 1, 1)
     else:
         return ackermann(m - 1, ackermann(m, n - 1))
+
+def ackermann_2(m, n):
+    while m >= 4:
+        if n == 0:
+            n = 1
+        else:
+            n = ackermann(m, n - 1)
+        m -= 1
+    if m == 3:
+        return (1 << n + 3) - 3
+    elif m == 2:
+        return (n << 1) + 3
+    elif m == 1:
+        return n + 2
+    else:
+        return n + 1
+
+if __name__ == "__main__":
+    ackermann_1(4, 2)
+    ackermann_2(4, 2)
