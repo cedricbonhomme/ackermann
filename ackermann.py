8cd8628258c5ea74b87750f15878370a37537ebc
cedricbonhomme <cedricbonhomme@localhost>
2007-02-15 17:38:25 +0100

    Error in the name of the function Ackermann for the Python implementation.

--- ackermann.py
+++ ackermann.py
@@ -7,16 +7,16 @@ def ackermann_1(m, n):
     if m == 0:
         return n + 1
     elif n == 0:
-        return ackermann(m - 1, 1)
+        return ackermann_1(m - 1, 1)
     else:
-        return ackermann(m - 1, ackermann(m, n - 1))
+        return ackermann_1(m - 1, ackermann_1(m, n - 1))
 
 def ackermann_2(m, n):
     while m >= 4:
         if n == 0:
             n = 1
         else:
-            n = ackermann(m, n - 1)
+            n = ackermann_2(m, n - 1)
         m -= 1
     if m == 3:
         return (1 << n + 3) - 3
