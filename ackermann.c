116cae908d4b66d1975afe21940598db5ea79539
cedricbonhomme <cedricbonhomme@localhost>
2012-09-29 18:16:53 +0200

    Added first C implementation of Ackermann.

---
+++ ackermann.c
@@ -0,0 +1,11 @@
+
+// Directly from definition
+unsigned int naive_ackermann(unsigned int m, unsigned int n) {
+    calls++;
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return naive_ackermann(m - 1, 1);
+    else
+        return naive_ackermann(m - 1, naive_ackermann(m, n - 1));
+}
