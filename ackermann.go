5fe6caf7bb03e1d1114570b612d936362c23a5f7
cedricbonhomme <cedricbonhomme@localhost>
2012-10-01 18:40:21 +0200

    Seems that there is a compiler bug. formule_ackermann should return something after the switch.

--- ackermann.go
+++ ackermann.go
@@ -17,9 +17,9 @@ func ackermann(m int, n int) int {
 }
 
 
-func formula_ackermann(m int, n int) int {
-    while (1) {
-        switch m { 
+func formula_ackermann(m int, n uint) uint {
+    for {
+        switch m {
             case 0:
                 return n + 1
             case 1:
@@ -28,15 +28,18 @@ func formula_ackermann(m int, n int) int {
                 return (n << 1) + 3
             case 3:
                 return (1 << (n+3)) - 3
+            default: {
+                if (n == 0) {
+                    n = 1
+                } else {
+                    n = formula_ackermann(m, n - 1)
+                }
+                m--
+                break
+            }
         }
-        if (n == 0) {
-            n = 1
-        } else {
-            n = formula_ackermann(m, n - 1)
-        }
-        m--
-        break
     }
+    return uint(42)
 }
 
 func main() {
@@ -45,5 +48,6 @@ func main() {
     m, _ := strconv.Atoi(argv[0])
     n, _ := strconv.Atoi(argv[1])
     fmt.Println(ackermann(m, n))
-    fmt.Println(formula_ackermann(m, n))
+    o := uint(n)
+    fmt.Println(formula_ackermann(m, o))
 }
