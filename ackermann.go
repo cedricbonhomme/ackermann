e9ce3ce5c4797ab4c88a3e593abd8c206104793f
cedricbonhomme <cedricbonhomme@localhost>
2012-10-01 13:35:00 +0200

    Added version of Ackermann with preevaluated values for the Go version.

--- ackermann.go
+++ ackermann.go
@@ -16,10 +16,34 @@ func ackermann(m int, n int) int {
     return ackermann(m-1, ackermann(m, n-1))
 }
 
+
+func formula_ackermann(m int, n int) int {
+    while (1) {
+        switch m { 
+            case 0:
+                return n + 1
+            case 1:
+                return n + 2
+            case 2:
+                return (n << 1) + 3
+            case 3:
+                return (1 << (n+3)) - 3
+        }
+        if (n == 0) {
+            n = 1
+        } else {
+            n = formula_ackermann(m, n - 1)
+        }
+        m--
+        break
+    }
+}
+
 func main() {
     flag.Parse()
     argv := flag.Args()
     m, _ := strconv.Atoi(argv[0])
     n, _ := strconv.Atoi(argv[1])
     fmt.Println(ackermann(m, n))
+    fmt.Println(formula_ackermann(m, n))
 }
